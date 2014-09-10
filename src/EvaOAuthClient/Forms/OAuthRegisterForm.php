4b1ac27fc53985108bb0bd9cba1807dcbd86d20d
AlloVince <allo.vince@gmail.com>
2014-09-10 17:54:09 +0800

    OAuth Register form comments

--- src/EvaOAuthClient/Forms/OAuthRegisterForm.php
+++ src/EvaOAuthClient/Forms/OAuthRegisterForm.php
@@ -1,5 +1,5 @@
 <?php
-namespace Eva\EvaUser\Forms;
+namespace Eva\EvaOAuthClient\Forms;
 
 use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
@@ -12,6 +12,7 @@ use Phalcon\Validation\Validator\Email;
 use Phalcon\Validation\Validator\Identical;
 use Phalcon\Validation\Validator\StringLength;
 use Phalcon\Validation\Validator\Confirmation;
+use Phalcon\Validation\Validator\Regex;
 
 /**
  * @package
@@ -45,108 +46,51 @@ class OAuthRegisterForm extends Form
     public function initialize($entity = null, $options = null)
     {
         $name = new Text('username');
-
         $name->setLabel('Name');
-
         $name->addValidators(array(
             new PresenceOf(array(
-                'message' => 'The name is required'
-            ))
+                'message' => 'Username is required'
+            )),
+            new Regex(array(
+                'pattern' => '/[0-9a-zA-Z_]+/',
+                'message' => 'Username is alphanumerics and underline only'
+            )),
+            new StringLength(array(
+                'min' => 4,
+                'max' => 24,
+                'messageMinimum' => 'Username is too short. Minimum 4 characters',
+                'messageMaximum' => 'Username is too long. Maximum 24 characters'
+            )),
         ));
-
         $this->add($name);
 
         // Email
         $email = new Text('email');
-
-        $email->setLabel('E-Mail');
-
+        $email->setLabel('Email');
         $email->addValidators(array(
             new PresenceOf(array(
-                'message' => 'The e-mail is required'
+                'message' => 'Email is required'
             )),
             new Email(array(
-                'message' => 'The e-mail is not valid'
+                'message' => 'Email is not valid'
             ))
         ));
-
         $this->add($email);
 
         // Password
         $password = new Password('password');
-
         $password->setLabel('Password');
-
         $password->addValidators(array(
             new PresenceOf(array(
                 'message' => 'The password is required'
             )),
             new StringLength(array(
-                'min' => 8,
-                'messageMinimum' => 'Password is too short. Minimum 8 characters'
+                'min' => 6,
+                'max' => 26,
+                'messageMinimum' => 'Password is too short. Minimum 6 characters',
+                'messageMaximum' => 'Password is too long. Maximum 26 characters'
             )),
-            new Confirmation(array(
-                'message' => 'Password doesn\'t match confirmation',
-                'with' => 'passwordConfirm'
-            ))
         ));
-
         $this->add($password);
-
-        // Confirm Password
-        $confirmPassword = new Password('passwordConfirm');
-
-        $confirmPassword->setLabel('Confirm Password');
-
-        $confirmPassword->addValidators(array(
-            new PresenceOf(array(
-                'message' => 'The confirmation password is required'
-            ))
-        ));
-
-        $this->add($confirmPassword);
-
-        // Remember
-        $terms = new Check('agree', array(
-            'value' => 'yes'
-        ));
-
-        $terms->setLabel('Accept terms and conditions');
-
-        $terms->addValidator(new Identical(array(
-            'value' => 'yes',
-            'message' => 'Terms and conditions must be accepted'
-        )));
-
-        $this->add($terms);
-
-        /*
-        // CSRF
-        $csrf = new Hidden('csrf');
-
-        $csrf->addValidator(new Identical(array(
-            'value' => $this->security->getSessionToken(),
-            'message' => 'CSRF validation failed'
-        )));
-
-        $this->add($csrf);
-
-        // Sign Up
-        $this->add(new Submit('Sign Up', array(
-            'class' => 'btn btn-success'
-        )));
-        */
-    }
-
-    /**
-     * Prints messages for a specific element
-     */
-    public function messages($name)
-    {
-        if ($this->hasMessagesFor($name)) {
-            foreach ($this->getMessagesFor($name) as $message) {
-                $this->flash->error($message);
-            }
-        }
     }
 }
